9951150c3d3b83e3c500c606e15822d9348c259b
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-27 19:09:29 +0200

    employee register project

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -2,14 +2,24 @@ package com.cydeo.controller;
 
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
 
     @GetMapping("/register")
     public String createEmployee(Model model){
@@ -19,5 +29,15 @@ public class EmployeeController {
 
         return "employee/employee-create";
     }
+
+
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") Employee employee){ // coming from Form
+
+        employeeService.saveEmployee(employee);
+
+        return "employee/employee-list";
+    }
+
 
 }
